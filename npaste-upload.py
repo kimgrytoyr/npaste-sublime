85b0d82a55d66b0c2d3e86f0c66e284ce9911f32
Kim Grytøyr <kim@grytoyr.io>
2019-02-17 11:13:48 +0100

    Initial commit

---
+++ npaste-upload.py
@@ -0,0 +1,57 @@
+import sublime
+import sublime_plugin
+import os
+import uuid
+import string
+import random
+import base64
+import requests
+from subprocess import *
+from requests.auth import HTTPBasicAuth
+
+class NpasteUploadCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    settings = sublime.load_settings('npaste.sublime-settings')
+    selection = self.view.sel()
+    selections = "\n".join(list(map(lambda s: self.view.substr(s), selection)))
+
+    if selections == "":
+      npaste_data = self.view.substr(sublime.Region(0, self.view.size()))
+    else:
+      npaste_data = selections
+
+    data = ""
+    passphrase = None
+    payload = {'age': settings.get('npaste.age') }
+
+    # Encrypt paste using GPG
+    if (settings.get('npaste.encrypt') == True):
+      passphrase = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(settings.get('npaste.encryption_key_length')))
+
+      process = Popen(['gpg', '--armor', '--batch', '--passphrase', passphrase, '--symmetric'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+      d = bytes(npaste_data, 'utf-8')
+      process.stdin.write(base64.b64encode(bytes(npaste_data, 'utf-8')))
+      paste, errs = process.communicate()
+
+      if (not errs):
+        data = paste.decode("utf-8")
+        payload["encrypted"] = 1
+      else:
+        sublime.message_dialog(errs.decode("utf-8"))
+    else:
+      data = npaste_data
+
+    if settings.get('npaste.archive') == True:
+      payload["archive"] = 1
+
+    # Upload paste to npaste
+    files = {'paste': ('paste.txt', data, 'text/plain')}
+    r = requests.post(settings.get('npaste.url'), files=files, data=payload, auth=HTTPBasicAuth(settings.get('npaste.username'), settings.get('npaste.password')))
+
+    url = r.text
+
+    if (passphrase is not None):
+      url = url + "#" + passphrase
+
+    sublime.set_clipboard(url)
+    sublime.message_dialog('Copied to clipboard!')
